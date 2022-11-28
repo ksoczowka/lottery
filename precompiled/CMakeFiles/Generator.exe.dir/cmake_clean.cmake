fa44956a25b728ff5e5becd9bf8f408f49965c68
ksoczowka <ksoczowka.ks@gmail.com>
2022-11-28 23:48:36 +0100

    Added windown .exe

---
+++ precompiled/CMakeFiles/Generator.exe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Generator.exe.dir/app/list-generator/main.cpp.o"
+  "CMakeFiles/Generator.exe.dir/app/list-generator/main.cpp.o.d"
+  "Generator.exe"
+  "Generator.exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Generator.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ precompiled/CMakeFiles/Lottery.exe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lottery.exe.dir/app/lottery/main.cpp.o"
+  "CMakeFiles/Lottery.exe.dir/app/lottery/main.cpp.o.d"
+  "Lottery.exe"
+  "Lottery.exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lottery.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
