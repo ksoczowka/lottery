fcdf2bfe1f44632eb97a6c6194827af75ef71dbd
ksoczowka <ksoczowka.ks@gmail.com>
2022-11-28 23:31:43 +0100

    Added precompiled cmake

---
+++ precompiled/CMakeFiles/Generator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Generator.dir/app/list-generator/main.cpp.o"
+  "CMakeFiles/Generator.dir/app/list-generator/main.cpp.o.d"
+  "Generator"
+  "Generator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Generator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ precompiled/CMakeFiles/Lottery.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lottery.dir/app/lottery/main.cpp.o"
+  "CMakeFiles/Lottery.dir/app/lottery/main.cpp.o.d"
+  "Lottery"
+  "Lottery.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lottery.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
